2ae3134d6187aae271b9a7f2a68b1ed819c7ff98
avidlaud <avidlaud@gmail.com>
2020-06-11 22:38:02 -0400

    Implement overlaying images

--- generator.py
+++ generator.py
@@ -4,9 +4,9 @@ from PIL import Image
 
 I = Image.open('./data/foreground/kh.png')
 
-plt.axis('off')
-plt.imshow(I)
-plt.show()
+#plt.axis('off')
+#plt.imshow(I)
+#plt.show()
 
 def scale_image(image, width, height, maintain_ratio=True):
     ''' Resizes an image to a given size, can maintain aspect ratio '''
@@ -19,8 +19,15 @@ def scale_image(image, width, height, maintain_ratio=True):
         image = image.resize((round(w*scale_factor), round(h*scale_factor)), Image.LANCZOS)
     return image
 
-def transform_image(image, min_width, min_height, max_width, max_height, maintain_ratio=True):
-    ''' Applies a random rotation and scale to image '''
-    image = image.rotate(random.randint(0, 359), expand=True)
-    image = scale_image(image, random.randint(min_width, max_width), random.randint(min_height, max_height), maintain_ratio)
-    return image
+def rotate_image(image):
+    ''' Applies a random rotation to the image '''
+    return image.rotate(random.randint(0, 359), expand=True)
+
+def overlay_image(image, background):
+    ''' Places image on the background '''
+    img_width, img_height = image.size
+    background_width, background_height = background.size
+    width_bound = background_width - img_width
+    height_bound = background_height - img_height
+    background.paste(image, (random.randint(0, width_bound), random.randint(0, height_bound)), image)
+    return background
